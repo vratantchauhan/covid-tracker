9101fe3f898de9a7a5ffca36bcc7e83a7e3b5f9a
vratantchauhan <31915365+vratantchauhan@users.noreply.github.com>
2021-05-21 01:08:47 -0500

    Add files via upload
    controller, service and model layer. Thymeleaf for the view layer.

---
+++ src/test/java/com/vratant/covidtracker/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vratant.covidtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
